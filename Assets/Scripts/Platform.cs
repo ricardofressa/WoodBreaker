acc2c20a2f16d324de558e0e87b96226f6cf2a85
Ricardo Cardoso <rick.duk@gmail.com>
2016-02-18 22:29:19 -0200

    Implemented movement platform

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class Platform : MonoBehaviour {
 
+	public float movementVelocity;
+	public float limitInX;
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		float horizontalDirection = Input.GetAxis ("Mouse X");
+		transform.position += Vector3.right * horizontalDirection * movementVelocity * Time.deltaTime;
+
+		float ActualX = transform.position.x;
+		ActualX = Mathf.Clamp (ActualX, -limitInX, limitInX);
+		transform.position = new Vector3 (ActualX, transform.position.y, transform.position.z);
 	}
 }
