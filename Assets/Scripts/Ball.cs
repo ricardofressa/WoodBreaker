9fb6179d84ea19c55296129d5ac0f50daa159ed9
Ricardo Cardoso <rick.duk@gmail.com>
2016-02-20 15:55:44 -0200

    Add Particle System Block in Ball Script

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,6 +5,7 @@ public class Ball : MonoBehaviour {
 
     public Vector3 direction;
     public float velocity;
+	public GameObject particleBlocks;
 
 	void Start ()
     {
@@ -42,6 +43,11 @@ public class Ball : MonoBehaviour {
 		else 
 		{
 			invalidCollider = false;
+			Bounds colliderBorder = collider.transform.GetComponent<SpriteRenderer> ().bounds;
+			Vector3 createPosition = new Vector3 (collider.transform.position.x + colliderBorder.extents.x, collider.transform.position.y - colliderBorder.extents.y, collider.transform.position.z);
+			GameObject particle = (GameObject) Instantiate (particleBlocks, createPosition, Quaternion.identity);
+			ParticleSystem particleComponent = particle.GetComponent<ParticleSystem> ();
+			Destroy (particle, particleComponent.duration + particleComponent.startLifetime);
 			Destroy (collider.gameObject);
 		}
 
